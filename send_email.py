e4c140313f41e12b7438b872a9bdabd8f58ba458
tranvohuy <39524209+tranvohuy@users.noreply.github.com>
2019-03-05 10:00:47 +0100

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -9,11 +9,13 @@ def send_email(ads_msgs):
     gmail_bot_pwd = environ['gmail_bot_pwd']
 
     sent_from = gmail_bot
-    to = environ['email_to']  
-    to = re.split(', ',to)
-    #example for the value in 'email_to': 'user1@amail.com,user2@gmai.com'
-    #each email is separated by a commas ',', no blank space.
-    #'user1@amail.com, user2@gmai.com' is not valid
+    to = environ['email_to']
+    to = to.replace(' ','')
+    to = re.split(',',to)
+    #example for the value in 'email_to': 'user1@amail.com, user2@gmai.com'
+    #each email is separated by a commas ','. Blank spaces are allowed
+    #'user1@amail.com,     user2@gmai.com' is also valid
+    #'user1@gmail.com,user2@dkjf.com' is also valid
     
     print(to)
     subject = 'Charlottenburg new rental ads'  
