605e940341ba99089c674f05c1dec2e438441c31
tranvohuy <39524209+tranvohuy@users.noreply.github.com>
2019-04-12 11:47:40 +0200

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -36,5 +36,5 @@ def send_email(ads_msgs):
         server.sendmail(sent_from, to, email_text.encode())
         server.close()
         print('Email sent.')
-    except:
-        print('Something went wrong...')
+    except Error as e:
+        print('Something went wrong with emails: ', e)
