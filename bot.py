3feec29b78082083a40dd2a505ccf6167df8bf58
tranvohuy <39524209+tranvohuy@users.noreply.github.com>
2019-04-12 11:48:39 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -106,7 +106,7 @@ if __name__=='__main__':
     print('ready to tweet')
     twitter_msgs = create_twitter_msgs(df_new)
     print(twitter_msgs)
-    update_tweet(twitter_msgs)
+   # update_tweet(twitter_msgs)
     
     email_msg = create_email_msg(df_new)
     print(email_msg)
